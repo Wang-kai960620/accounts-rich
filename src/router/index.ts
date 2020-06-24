3a975c0894b80d19016eb1b5ef950e4a13f04f95
wangkai <wk.dong.up@gmail.com>
2020-06-24 15:24:41 +0800

    添加404页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import VueRouter, {RouteConfig} from "vue-router";
 import Money from "@/views/Money.vue";
 import Labels from "@/views/Labels.vue";
 import Statistics from "@/views/Statistics.vue";
+import NotFond from "@/views/NotFond.vue";
 
 Vue.use(VueRouter);
 
@@ -24,8 +25,11 @@ const routes: Array<RouteConfig> = [
   {
     path: "/statistics",
     component: Statistics
+  },
+  {
+    path: "*",
+    component: NotFond
   }
-
 ];
 
 const router = new VueRouter({
