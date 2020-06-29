329329baee744387dea5caf39cd4c2bc88ba9dc7
wangkai <wk.dong.up@gmail.com>
2020-06-29 15:56:41 +0800

    基本完成numberPad组件

--- src/components/Number/NumberButton.vue
+++ src/components/Number/NumberButton.vue
@@ -1,59 +1,58 @@
 <template>
-    <div class="numberMap">
-        <div>
-            <button>7</button>
-        </div>
-        <div>
-            <button>8</button>
-        </div>
-        <div>
-            <button>9</button>
-        </div>
-        <div>
-            <button>回退</button>
-        </div>
-        <div>
-            <button>4</button>
-        </div>
-        <div>
-            <button>5</button>
-        </div>
-        <div>
-            <button>6</button>
-        </div>
-        <div>
-            <button>删除</button>
-        </div>
-        <div>
-            <button>1</button>
-        </div>
-        <div>
-            <button>2</button>
-        </div>
-        <div>
-            <button>3</button>
-        </div>
-        <div class="add">
-            <button class="aButton">
-                <router-link to="/">
-
-                OK
-            </router-link>
-            </button>
-
-
-        </div>
-        <div>
-            <button>.</button>
-        </div>
-        <div>
-            <button>0</button>
-        </div>
-        <div>
-            <button>#</button>
+    <div>
+        <div class="show">{{output}}</div>
+        <div class="numberMap">
+            <div>
+                <button @click="inputContent">7</button>
+            </div>
+            <div>
+                <button @click="inputContent">8</button>
+            </div>
+            <div>
+                <button @click="inputContent">9</button>
+            </div>
+            <div>
+                <button @click="remove">删除</button>
+            </div>
+            <div>
+                <button @click="inputContent">4</button>
+            </div>
+            <div>
+                <button @click="inputContent">5</button>
+            </div>
+            <div>
+                <button @click="inputContent">6</button>
+            </div>
+            <div>
+                <button @click="clear">清空</button>
+            </div>
+            <div>
+                <button @click="inputContent">1</button>
+            </div>
+            <div>
+                <button @click="inputContent">2</button>
+            </div>
+            <div>
+                <button @click="inputContent">3</button>
+            </div>
+            <div class="add">
+                <button class="aButton">
+                    <router-link to="/">
+                        OK
+                    </router-link>
+                </button>
+            </div>
+            <div>
+                <button @click="inputContent">.</button>
+            </div>
+            <div>
+                <button @click="inputContent">0</button>
+            </div>
+            <div>
+                <button>#</button>
+            </div>
         </div>
     </div>
-
 </template>
 
 <script lang="ts">
@@ -62,13 +61,47 @@
 
 
   @Component
-  export default class NumberButton extends Vue{
+  export default class NumberButton extends Vue {
+    output = "0";
 
+    inputContent(event: MouseEvent) {
+      const button = (event.target as HTMLButtonElement);
+      const input = button.textContent!;
+      if (this.output.length >= 16) {return;}
+      if (this.output === "0") {
+        if ("0123456789".indexOf(input) >= 0) {
+          this.output = input;
+        } else {
+          this.output += input;
+        }
+        return;
+      }
+      if (this.output.indexOf(".") >= 0 && input === ".") {return;}
+      this.output += input;
+    }
+    remove(){
+      if(this.output.length===1){
+        this.output = '0'
+      }else{
+        this.output = this.output.slice(0,-1)
+      }
+    }
+    clear(){
+      this.output = '0'
+    }
   }
 </script>
 
 <style lang="scss" scoped>
     @import "~@/assets/style/help.scss";
+
+    .show {
+        font-size: 46px;
+        text-align: right;
+        padding: 20px;
+        font-family: Consolas, monospace;
+    }
+
 
     .numberMap {
         margin: auto;
@@ -90,7 +123,9 @@
                 border-radius: 50px;
                 box-shadow: 0 0 3px rgba(0, 0, 0, 0.3);
 
+
             }
+
 
         }
 
--- src/views/NumberMap.vue
+++ src/views/NumberMap.vue
@@ -2,23 +2,19 @@
     <layout>
         <Title/>
         <Type/>
-        <Show/>
         <NumberButton/>
     </layout>
 </template>
 
 
-
-
 <script lang="ts">
   import Title from "@/components/Number/Title.vue";
   import Type from "@/components/Number/Type.vue";
-  import Show from "@/components/Number/Show.vue";
   import NumberButton from "@/components/Number/NumberButton.vue";
 
   export default {
     name: "NumberMap",
-    components: {Show, Type, Title , NumberButton}
+    components: {Type, Title, NumberButton}
   };
 </script>
 
