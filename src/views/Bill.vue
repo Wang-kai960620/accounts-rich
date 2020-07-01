beaecb9a5299cdb276fd099795cf38da01c3a446
wangkai <wk.dong.up@gmail.com>
2020-07-01 14:01:07 +0800

    完成了bill界面css样式

--- src/views/Bill.vue
+++ src/views/Bill.vue
@@ -5,24 +5,37 @@
                 year</label>
             <label class="mouths">{{n}}</label>
             <div class="input">收 入
-            <div>0.00</div>
+                <div>0.00</div>
             </div>
             <div class="output">支 出
-            <div>0.00</div>
+                <div>0.00</div>
             </div>
         </div>
-        <div>walk</div>
+        <div class="inTag" @click="toDetail">
+            <div class="outText">支出</div>
+            <div class="outList">衣 <span>100</span></div>
+        </div>
+        <div class="outTag" @click="toDetail">
+            <div class="inText">收入</div>
+            <div class="inList">工资 <span>100</span></div>
+        </div>
     </Layout>
 </template>
 
 <script lang="ts">
-    import Vue from 'vue'
-    import {Component} from "vue-property-decorator";
-    @Component
-export default class Bill   extends Vue{
-  n='6月'
+  import Vue from "vue";
+  import router from "@/router";
+  import {Component} from "vue-property-decorator";
 
-}
+  @Component
+  export default class Bill extends Vue {
+    n = "6月";
+
+    toDetail() {
+      router.push("detail");
+    }
+
+  }
 </script>
 
 <style lang="scss" scoped>
@@ -43,24 +56,89 @@ export default class Bill   extends Vue{
 
         > .mouths {
             position: absolute;
-            font-size:56px;
+            font-size: 56px;
             bottom: 0;
             left: 30px;
-            font-family: monospace,Consolas;
+            font-family: monospace, Consolas;
         }
 
         > .output {
             position: absolute;
             left: 70%;
             bottom: 0;
-            font-family: Consolas,monospace;
+            font-family: Consolas, monospace;
         }
 
         > .input {
             position: absolute;
             left: 40%;
             bottom: 0;
-            font-family: Consolas,monospace;
+            font-family: Consolas, monospace;
+        }
+    }
+
+    .inTag {
+        margin: 20px 0;
+        font-size: 26px;
+        height: 30vh;
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+        overflow: hidden;
+
+        > .outList {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            width: 100%;
+            height: 8vh;
+            padding: 0 10px;
+            margin: 5px 0;
+            border-radius: 30px;
+            background: white;
+            color: black;
+            box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+
+        }
+
+
+        > .outText {
+            height: 50px;
+            padding: 3px 5px;
+            border-radius: 10px;
+            background: #ffcd00;
+        }
+
+    }
+
+    .outTag {
+        margin: 20px 0;
+        font-size: 26px;
+        height: 30vh;
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+        overflow: hidden;
+
+        > .inList {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            width: 100%;
+            height: 8vh;
+            padding: 0 10px;
+            margin: 5px 0;
+            border-radius: 30px;
+            background: white;
+            color: black;
+            box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+        }
+
+        > .inText {
+            height: 50px;
+            padding: 3px 5px;
+            border-radius: 10px;
+            background: #ffcd00;
         }
     }
 
