eac1e817dda5ded925b16f08f4a20c2bf32b215f
wangkai <wk.dong.up@gmail.com>
2020-07-17 21:50:43 +0800

    添加支出收入符号

--- src/views/Bill.vue
+++ src/views/Bill.vue
@@ -1,141 +1,151 @@
 <template>
-    <Layout>
-        <div class="billTitle">
-            <label class="year">
-                {{year}}年</label>
-            <label class="mouths">{{n}}月</label>
-            <div class="input">收 入
-                <div>{{inMoney}}元</div>
-            </div>
-            <div class="output">支 出
-                <div>{{outMoney}}元</div>
-            </div>
-        </div>
-        <div class="outTag">
-            <div class="outList" @click="toDetail"
-                 v-for="(name,index) in moneyList" :key="index">
-                <Icon :name="name.tags" class="icon"/>
-                ￥{{name.amount}}
-            </div>
-        </div>
-    </Layout>
+  <Layout>
+    <div class="billTitle">
+      <label class="year">{{year}}年</label>
+      <label class="mouths">{{n}}月</label>
+      <div class="input">
+        收 入
+        <div>{{inMoney}}元</div>
+      </div>
+      <div class="output">
+        支 出
+        <div>{{outMoney}}元</div>
+      </div>
+    </div>
+    <div class="outTag">
+      <div class="outList" @click="toDetail" v-for="(name,index) in moneyList" :key="index">
+        <Icon :name="name.tags" class="icon" />
+        {{name.type}}￥{{name.amount}}
+      </div>
+    </div>
+  </Layout>
 </template>
 
 <script lang="ts">
-  import Vue from "vue";
-  import router from "@/router";
-  import {Component} from "vue-property-decorator";
-  import clone from "@/lib/clone";
-  import dayjs from "dayjs";
+import Vue from "vue";
+import router from "@/router";
+import { Component } from "vue-property-decorator";
+import clone from "@/lib/clone";
+import dayjs from "dayjs";
 
-  type RootItem = {
-    recordList: RecordItem[];
+type RootItem = {
+  recordList: RecordItem[];
+};
+
+@Component
+export default class Bill extends Vue {
+  year = "";
+  inMoney = "0.00";
+  outMoney = "0.00";
+  n = "";
+
+  toDetail() {
+    router.push("detail");
   }
 
-  @Component
-  export default class Bill extends Vue {
-    year = "";
-    inMoney = "0.00";
-    outMoney = "0.00";
-    n = "";
-
-
-    toDetail() {
-      router.push("detail");
-    }
-
-    mounted() {
-      this.$store.commit("fetchRecords");
-      const years = new Date().getUTCFullYear();
-      this.year = years.toString();
-      const month = new Date().getMonth() + 1;
-      this.n = month.toString();
-    }
-
-    get recordList() {
-      return (this.$store.state as RootItem).recordList;
-    }
-
-    get moneyList() {
-      const {recordList} = this;
-      if (recordList.length === 0) {return [] }
-      const newList = clone(recordList).sort((a, b) => dayjs(b.timeAt).valueOf() - dayjs(a.timeAt).valueOf());
-      const nowMonth = newList.filter(item => dayjs(item.timeAt).isSame(dayjs().valueOf(), "year"));
-      const nowMonth2 = nowMonth.filter(item => dayjs(item.timeAt).isSame(dayjs().valueOf(), "month"));
-      this.inMoney = nowMonth2.filter(item => item.type === "+").reduce((sum, items) => {return sum + items.amount;}, 0).toString();
-      this.outMoney = nowMonth2.filter(item => item.type === "-").reduce((sum, items) => {return sum + items.amount;}, 0).toString();
-      return nowMonth2;
-    }
-
+  mounted() {
+    this.$store.commit("fetchRecords");
+    const years = new Date().getUTCFullYear();
+    this.year = years.toString();
+    const month = new Date().getMonth() + 1;
+    this.n = month.toString();
   }
+
+  get recordList() {
+    return (this.$store.state as RootItem).recordList;
+  }
+
+  get moneyList() {
+    const { recordList } = this;
+    if (recordList.length === 0) {
+      return [];
+    }
+    const newList = clone(recordList).sort(
+      (a, b) => dayjs(b.timeAt).valueOf() - dayjs(a.timeAt).valueOf()
+    );
+    const nowMonth = newList.filter(item =>
+      dayjs(item.timeAt).isSame(dayjs().valueOf(), "year")
+    );
+    const nowMonth2 = nowMonth.filter(item =>
+      dayjs(item.timeAt).isSame(dayjs().valueOf(), "month")
+    );
+    this.inMoney = nowMonth2
+      .filter(item => item.type === "+")
+      .reduce((sum, items) => {
+        return sum + items.amount;
+      }, 0)
+      .toString();
+    this.outMoney = nowMonth2
+      .filter(item => item.type === "-")
+      .reduce((sum, items) => {
+        return sum + items.amount;
+      }, 0)
+      .toString();
+    return nowMonth2;
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-    .billTitle {
-        height: 26vh;
-        display: flex;
-        position: relative;
-        font-size: 26px;
-        border-radius: 0 0 20px 20px;
-        background: #ffcd00;
-        box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+.billTitle {
+  height: 26vh;
+  display: flex;
+  position: relative;
+  font-size: 26px;
+  border-radius: 0 0 20px 20px;
+  background: #ffcd00;
+  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
 
-        > .year {
-            position: absolute;
-            left: 30px;
-            font-size: 30px;
-            padding: 10px 0;
-        }
+  > .year {
+    position: absolute;
+    left: 30px;
+    font-size: 30px;
+    padding: 10px 0;
+  }
 
-        > .mouths {
-            position: absolute;
-            font-size: 50px;
-            bottom: 0;
-            left: 30px;
-            font-family: monospace, Consolas;
-        }
+  > .mouths {
+    position: absolute;
+    font-size: 50px;
+    bottom: 0;
+    left: 30px;
+    font-family: monospace, Consolas;
+  }
 
-        > .output {
-            position: absolute;
-            left: 70%;
-            bottom: 0;
-            font-family: Consolas, monospace;
-        }
+  > .output {
+    position: absolute;
+    left: 70%;
+    bottom: 0;
+    font-family: Consolas, monospace;
+  }
 
-        > .input {
-            position: absolute;
-            left: 40%;
-            bottom: 0;
-            font-family: Consolas, monospace;
-        }
-    }
+  > .input {
+    position: absolute;
+    left: 40%;
+    bottom: 0;
+    font-family: Consolas, monospace;
+  }
+}
 
+.outTag {
+  margin: 20px 0;
+  font-size: 26px;
+  height: 60vh;
+  flex-wrap: wrap;
+  overflow: scroll;
 
-    .outTag {
-        margin: 20px 0;
-        font-size: 26px;
-        height: 60vh;
-        flex-wrap: wrap;
-        overflow: scroll;
-
-        > .outList {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            width: 100%;
-            height: 8vh;
-            padding: 0 20px;
-            margin: 5px 0;
-            border-radius: 30px;
-            background: white;
-            color: black;
-            box-shadow: 0 4px 4px rgba(0, 0, 0, 0.05);
-            font-size: 26px;
-
-        }
-
-
-    }
-
-
+  > .outList {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    height: 8vh;
+    padding: 0 20px;
+    margin: 5px 0;
+    border-radius: 30px;
+    background: white;
+    color: black;
+    box-shadow: 0 4px 4px rgba(0, 0, 0, 0.05);
+    font-size: 26px;
+  }
+}
 </style>
--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,121 +1,129 @@
 <template>
-    <Layout>
-        <detailTitle/>
-        <DetailButton :value="type" @update:value="onchange"/>
+  <Layout>
+    <detailTitle />
+    <DetailButton :value="type" @update:value="onchange" />
 
-        <ol v-for="(group,index) in groupList" :key="index" class="list">
-            <li class="time">{{group.title}}<span>￥{{group.total}}</span></li>
-            <li v-for="(item,index) in group.items" :key="index" class="listLi">
-                <Icon :name="item.tags" class="icon"/>
-                <span class="remark">{{item.notes}}</span>
-                <span>￥{{item.amount}}</span>
-            </li>
-        </ol>
-    </Layout>
+    <ol v-for="(group,index) in groupList" :key="index" class="list">
+      <li class="time">
+        {{group.title}}
+        <span>￥{{group.total}}</span>
+      </li>
+      <li v-for="(item,index) in group.items" :key="index" class="listLi">
+        <Icon :name="item.tags" class="icon" />
+        <span class="remark">{{item.notes}}</span>
+        <span>{{item.type}}￥{{item.amount}}</span>
+      </li>
+    </ol>
+  </Layout>
 </template>
 
 
 <script lang="ts">
-  import Vue from "vue";
-  import detailTitle from "@/components/Detail/detailTitle.vue";
-  import DetailButton from "@/components/Detail/DetailButton.vue";
-  import {Component} from "vue-property-decorator";
-  import clone from "@/lib/clone";
-  import dayjs from "dayjs";
+import Vue from "vue";
+import detailTitle from "@/components/Detail/detailTitle.vue";
+import DetailButton from "@/components/Detail/DetailButton.vue";
+import { Component } from "vue-property-decorator";
+import clone from "@/lib/clone";
+import dayjs from "dayjs";
 
-  type RootItem = {
-    recordList: RecordItem[];
+type RootItem = {
+  recordList: RecordItem[];
+};
+
+@Component({ components: { DetailButton, detailTitle } })
+export default class Detail extends Vue {
+  type = "-";
+
+  created() {
+    this.$store.commit("fetchRecords");
   }
 
-  @Component({components: {DetailButton, detailTitle}})
-  export default class Detail extends Vue {
-    type = "-";
+  get recordList() {
+    return (this.$store.state as RootItem).recordList;
+  }
 
-    created() {
-      this.$store.commit("fetchRecords");
+  get groupList() {
+    const { recordList } = this;
+    if (recordList.length === 0) {
+      return [];
     }
-
-    get recordList() {
-      return (this.$store.state as RootItem).recordList;
-    }
-
-    get groupList() {
-      const {recordList} = this;
-      if (recordList.length === 0) {return [];}
-      const newList = clone(recordList).filter(item => item.type === this.type)
-        .sort((a, b) => dayjs(b.timeAt).valueOf() - dayjs(a.timeAt).valueOf());
-      type Result = { title: string; total?: number; items: RecordItem[] }[]
-      const result: Result = [{title: dayjs(newList[0].timeAt).format("YYYY-MM-DD"), items: [newList[0]]}];
-      for (let i = 1; i < newList.length; i++) {
-        const current = newList[i];
-        const last = result[result.length - 1];
-        if (dayjs(last.title).isSame(dayjs(current.timeAt), "day")) {
-          last.items.push(current);
-        } else {
-          result.push({title: dayjs(current.timeAt).format("YYYY-MM-DD"), items: [current]});
-        }
-
+    const newList = clone(recordList)
+      .filter(item => item.type === this.type)
+      .sort((a, b) => dayjs(b.timeAt).valueOf() - dayjs(a.timeAt).valueOf());
+    type Result = { title: string; total?: number; items: RecordItem[] }[];
+    const result: Result = [
+      {
+        title: dayjs(newList[0].timeAt).format("YYYY-MM-DD"),
+        items: [newList[0]]
       }
-      result.map(group => {
-        group.total = group.items.reduce((sum, item) => {
-          return sum + item.amount;
-        }, 0);
-      });
-      return result;
+    ];
+    for (let i = 1; i < newList.length; i++) {
+      const current = newList[i];
+      const last = result[result.length - 1];
+      if (dayjs(last.title).isSame(dayjs(current.timeAt), "day")) {
+        last.items.push(current);
+      } else {
+        result.push({
+          title: dayjs(current.timeAt).format("YYYY-MM-DD"),
+          items: [current]
+        });
+      }
     }
-
-    onchange(value: string) {
-      this.type = value;
-    }
+    result.map(group => {
+      group.total = group.items.reduce((sum, item) => {
+        return sum + item.amount;
+      }, 0);
+    });
+    return result;
   }
+
+  onchange(value: string) {
+    this.type = value;
+  }
+}
 </script>
 
 <style lang="scss" scoped>
+.list {
+  max-height: 60vh;
+  overflow: scroll;
+  font-size: 25px;
+  display: flex;
+  flex-wrap: wrap;
+  padding: 5px 10px;
 
-    .list {
-        max-height: 60vh;
-        overflow: scroll;
-        font-size: 25px;
-        display: flex;
-        flex-wrap: wrap;
-        padding: 5px 10px;
+  > .time {
+    display: flex;
+    justify-content: space-between;
+    background: white;
+    width: 100%;
 
-        > .time {
-            display: flex;
-            justify-content: space-between;
-            background: white;
-            width: 100%;
-
-            > span {
-                padding: 0 15px;
-
-            }
-
-        }
-
-        > .listLi {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            margin: 2px 0;
-            width: 100%;
-            list-style: none;
-            border-radius: 30px 30px 30px 30px;
-            padding: 0 15px;
-            box-shadow: 0 4px 4px rgba(0, 0, 0, 0.05);
-            background: white;
-
-        }
-
-        > .icon {
-            width: 50px;
-            height: 50px;
-        }
+    > span {
+      padding: 0 15px;
     }
+  }
 
-    .remark {
-        font-size: 20px;
-        color: #c4c4c4;
-    }
+  > .listLi {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 2px 0;
+    width: 100%;
+    list-style: none;
+    border-radius: 30px 30px 30px 30px;
+    padding: 0 15px;
+    box-shadow: 0 4px 4px rgba(0, 0, 0, 0.05);
+    background: white;
+  }
 
+  > .icon {
+    width: 50px;
+    height: 50px;
+  }
+}
+
+.remark {
+  font-size: 20px;
+  color: #c4c4c4;
+}
 </style>
